cbe8d4bb9275fffb54a2154a4e275701215c9a98
koopa1338 <sinner1991@gmail.com>
2021-10-06 20:47:21 +0200

    Further API Design
    - Own error type
    - traits for Role and Policy
    - trait for a SSOConnector
    
    The SSOConnector trait requires the used types for role and policy to
    implement the corresponding traits. This enables the user to use his own
    types for roles and policies.
    
    There will be default implementations for keycloak, authelia and CAS but
    any SSO service can be easily extended by implementing the traits.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,55 @@
-use std::sync::Arc;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct Role {
-    id: usize,
-    pub name: String,
+enum SSOErrorKind {
+    SSOConnectionError,
 }
 
-#[derive(Debug, Clone)]
-struct Policy {
-    id: usize,
-    pub name: String,
-    pub roles: Arc<Vec<Role>>,
-    // maybe save the rule but how does that look like?
+struct SSOError {
+    kind: SSOErrorKind,
+    message: String,
 }
 
-impl Policy {
-    pub fn role_is_authorized(&self, role: Role) -> bool {
-        self.roles.contains(&role)  
+trait Role {
+}
+
+trait Policy {
+    type R: Role;
+
+    fn role_is_authorized(&self, role: Self::R) -> bool;
+}
+
+trait SSOConnector {
+    type R: Role;
+    type P: Policy;
+
+    fn connect(&self, user: String, password: String) -> Result<bool, SSOError>;
+
+    fn get_roles(&self) -> Vec<Self::R>;
+
+    fn get_policies(&self) -> Vec<Self::P>;
+}
+
+struct SSOConfig<S> where S: SSOConnector {
+    url: String,
+    server: S,
+    user: String,
+    password: String,
+    roles: Option<Vec<<S as SSOConnector>::R>>,
+    policies: Option<Vec<<S as SSOConnector>::P>>,
+}
+
+impl<S> SSOConfig<S>
+where S: SSOConnector
+{
+    fn new(url: String, server: S, user: String, password: String) -> Self {
+        let roles = server.get_roles();
+        let policies = server.get_policies();
+        SSOConfig {
+            url,
+            server,
+            user,
+            password,
+            roles: Some(roles),
+            policies: Some(policies),
+        }
     }
+ 
 }
