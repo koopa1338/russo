9aa9920f4784364578ff3245b9f1c7764f17d48f
koopa1338 <sinner1991@gmail.com>
2021-11-05 13:38:22 +0100

    added possible error kinds for SSOError (not final)

--- src/russoerror.rs
+++ src/russoerror.rs
@@ -1,5 +1,8 @@
 pub enum SSOErrorKind {
     SSOConnectionError,
+    SSOUnreachableError,
+    SSOUnsopportedError,
+    SSOMalformedRequestError,
 }
 
 /// Error that is returned if communication with the sso server fails. See SSOErrorKind for
