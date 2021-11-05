6292c26fe30c07d2c5a183a6071c7342c86c2023
koopa1338 <sinner1991@gmail.com>
2021-11-05 13:38:13 +0100

    Added some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,16 @@ mod server;
 
 use russoerror::SSOError;
 
+/// A SSO connector trait that is generic over a server authentication type A and has a associated
+/// type R that represents a role to enable role based permissions
+/// The authentication type is a struct that represents a authentication state but could contain
+/// see one of the default implementations.
 trait SSOConnector<A> {
     type R: Clone;
 
+    /// Returns the authentication type A if succesful or a SSOError if authentication fails.
     fn authenticate(&mut self) -> Result<A, SSOError>;
 
+    /// Returns a vec of the associated role type or a SSOError if the request fails.
     fn roles(&mut self) -> Result<Vec<Self::R>, SSOError>;
 }
--- src/russoerror.rs
+++ src/russoerror.rs
@@ -2,6 +2,8 @@ pub enum SSOErrorKind {
     SSOConnectionError,
 }
 
+/// Error that is returned if communication with the sso server fails. See SSOErrorKind for
+/// possible erros.
 pub struct SSOError {
     kind: SSOErrorKind,
     message: String,
